e245385073b02d7e24d67311b467f04d2413dc04
Pranit <123458975+Pranit223@users.noreply.github.com>
2024-05-05 20:49:00 +0530

    Update Conn.js

--- DATABASE_CONNECTOR/backend/DB/Conn.js
+++ DATABASE_CONNECTOR/backend/DB/Conn.js
@@ -1,19 +1,21 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+
 
 const connectDB = async () => {
-    const URI = process.env.MONGO_URI;
-    try {
-        if (!URI) {
-            throw new Error("mongodb+srv://singhrupanjay:XpzOpxSdau5Tpjmh@jobmanchcluster.jcqys1e.mongodb.net/");
-        }
-
-        const connect = await mongoose.connect(URI, {
-            dbName: "For_Job_Manch",
-        });
-        console.log("Connected To mongoDB");
-    } catch (error) {
-        console.log(`ERROR IS ${error}`);
+  const URI =
+    "mongodb+srv://pranitaakash211:9K50JW0ko60l1yFw@cluster0.ono7yl4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  try {
+    if (!URI) {
+      throw new Error("missing");
     }
+
+    const connect = await mongoose.connect(URI, {
+      dbName: "For_Job_Manch",
+    });
+    console.log("Connected To mongoDB");
+  } catch (error) {
+    console.log(`ERROR IS ${error}`);
+  }
 };
 
-export { connectDB };
+module.exports.connectDB = connectDB;
